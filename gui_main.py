61be7bf7696042a2cae8506f8c82c877efd1c481
Ľuboš Dragan <lubos@MacBook-Air.local>
2024-01-02 17:36:28 +0100

    Otacanie lodi

--- gui_main.py
+++ gui_main.py
@@ -37,18 +37,21 @@ class Window(arcade.Window):
                     and y >= lod.LHY - self.stvorec_size * lod.size
                 ) and not self.aktivna_lod:
                     self.aktivna_lod = lod
+                    self.aktivna_lod.alpha = 170
                     print("asdas")
         if self.hra_sa and self.aktivne_policko:
             self.aktivne_policko.zasah()
             self.aktivne_policko = None
             arcade.cleanup_texture_cache()
-
         
     def on_mouse_motion(self, x, y, dx, dy):
+        if self.aktivna_lod:
+            self.aktivna_lod.vykresli(x,y)
+        
         nove_pole = None
         if x < self.stvorec_size * (len(self.hracie_pole) + 1) + 100:
             nove_pole = self.zisti_policko_pod_mysou(x, y, self.hracie_pole)
-        elif x > self.width - (self.stvorec_size * (len(self.nepriatelske_pole) + 1)) - 100:
+        elif x > self.width - (self.stvorec_size * (len(self.nepriatelske_pole) + 1)) - 100 and not self.aktivna_lod:
             nove_pole = self.zisti_policko_pod_mysou(x, y, self.nepriatelske_pole)
             
         if nove_pole:
@@ -66,8 +69,16 @@ class Window(arcade.Window):
         arcade.cleanup_texture_cache()
         super().on_close()
         
-    def nacitaj_lode(self):
-            self.hrac_lode = [
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.R and self.aktivna_lod:
+            self.aktivna_lod.otoc()
+            
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_RIGHT and self.aktivna_lod:
+            self.aktivna_lod.otoc()
+        
+    def priprav_lode(self):
+        self.hrac_lode = [
                 Lod(5, "./assets/ship1.png"),
                 Lod(4, "./assets/ship2.png"),
                 Lod(4, "./assets/ship3.png"),
@@ -76,19 +87,26 @@ class Window(arcade.Window):
                 Lod(2, "./assets/ship6.png"),
                 Lod(2, "./assets/ship7.png"),
             ]
-            index = 1
-            for lod in self.hrac_lode:
-                lod.vykresli(index * (stvorec_size * 1 + 20), None)
-                index += 1
+        index = 1
+        for lod in self.hrac_lode:
+            lod.vykresli(index * (stvorec_size * 1 + 20), None)
+            index += 1   
+
+    def nacitaj_lode(self):
+        if len(self.hrac_lode) < 1:
+            self.priprav_lode()
+        for lod in self.hrac_lode:
+            lod.vykresli(None, None)
+        
     
     def zisti_policko_pod_mysou(self, x, y, pole):
         for row in pole:
             for policko in row:
                 stvorec = policko.stvorec
                 if (
-                    x > stvorec.x
+                    x >= stvorec.x
                     and x < stvorec.x + stvorec.size
-                    and y > stvorec.y
+                    and y >= stvorec.y
                     and y < stvorec.y + stvorec.size
                 ):
                     return policko
@@ -111,6 +129,7 @@ class Lod:
         self.angle = 0
         self.LHX = 0
         self.LHY = 0
+        self.alpha = 255
 
     def vykresli(self, x, y):
         if x or x == 0 : self.x = x
@@ -118,9 +137,10 @@ class Lod:
         if self.LHX == 0: self.LHX = self.x - (stvorec_size / 2)
         if self.LHY == 0: self.LHY = self.y + (self.size * stvorec_size / 2)
         if self.image:
-            arcade.draw_texture_rectangle(self.x, self.y, stvorec_size, stvorec_size * self.size, self.image, self.angle)
+            arcade.draw_texture_rectangle(self.x, self.y, stvorec_size, stvorec_size * self.size, self.image, angle=self.angle, alpha=self.alpha)
         else:
             print("Nepodarilo sa nacitat obrazok lode")
             
     def otoc(self):
-        self.rotation = 90 if self.rotation == 0 else 0
+        self.angle = 90 if self.angle == 0 else 0
+        print(self.angle)
